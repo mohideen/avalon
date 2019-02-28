2b3cbd58031e6a9a6364a5a96bdbb70cf45ba745
Mohamed Abdul Rasheed <mohideen@umd.edu>
2019-02-28 15:45:26 -0500

    Resolve email error

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,4 +1,7 @@
-if Settings.email.mailer.present? && Settings.email.mailer == :aws_sdk
-  require 'aws/rails/mailer'
+if File.exist?('/sys/hypervisor/uuid') && (File.read('/sys/hypervisor/uuid',3) == 'ec2')
+  require 'aws-sdk-rails'
+  Aws::Rails.add_action_mailer_delivery_method(:aws_sdk)
   ActionMailer::Base.delivery_method = :aws_sdk
+  ActionMailer::Base.raise_delivery_errors = false
+  ActionMailer::DeliveryJob.queue_as Settings.active_job.queues.ingest
 end
